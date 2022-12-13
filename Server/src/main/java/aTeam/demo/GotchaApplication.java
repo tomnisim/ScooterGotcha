3c84343d0c2490bcfb61459d3b17d3c3a71c68d0
amitmosk <63066271+amitmosk@users.noreply.github.com>
2022-12-13 15:45:33 +0200

    add server files

---
+++ Server/src/main/java/aTeam/demo/GotchaApplication.java
@@ -0,0 +1,13 @@
+package aTeam.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotchaApplication.class, args);
+	}
+
+}
