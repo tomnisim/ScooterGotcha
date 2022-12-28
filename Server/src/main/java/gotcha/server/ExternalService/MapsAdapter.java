15f7eae7d4f54d2498dfbfb2b586ff3ff21de0f8
amitmosk <63066271+amitmosk@users.noreply.github.com>
2022-12-28 14:43:32 +0200

    external support to get routes

--- Server/src/main/java/gotcha/server/ExternalService/MapsAdapter.java
+++ Server/src/main/java/gotcha/server/ExternalService/MapsAdapter.java
@@ -1,6 +1,12 @@
 package gotcha.server.ExternalService;
 
+import gotcha.server.SafeRouteCalculatorModule.Route;
+import gotcha.server.Utils.Location;
+
+import java.util.List;
+
 public interface MapsAdapter {
     boolean handshake();
+    List<Route> get_routes(Location source, Location destination, int number_of_routes);
 
 }
--- Server/src/main/java/gotcha/server/ExternalService/MapsAdapterImpl.java
+++ Server/src/main/java/gotcha/server/ExternalService/MapsAdapterImpl.java
@@ -1,8 +1,18 @@
 package gotcha.server.ExternalService;
 
+import gotcha.server.SafeRouteCalculatorModule.Route;
+import gotcha.server.Utils.Location;
+
+import java.util.List;
+
 public class MapsAdapterImpl implements MapsAdapter {
     @Override
     public boolean handshake() {
         return false;
+    }
+
+    @Override
+    public List<Route> get_routes(Location source, Location destination, int number_of_routes) {
+        return null;
     }
 }
