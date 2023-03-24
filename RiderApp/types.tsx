d5e5a207804ca9b6329e174abeb69897a8df473d
tomnisim <tomnisi@post.bgu.ac.il>
2023-03-24 16:11:38 +0300

    added home page

--- RiderApp/types.tsx
+++ RiderApp/types.tsx
@@ -17,6 +17,7 @@ export type RootStackParamList = {
   Root: NavigatorScreenParams<RootTabParamList> | undefined;
   Modal: undefined;
   NotFound: undefined;
+  Button: undefined
 };
 
 export type RootStackScreenProps<Screen extends keyof RootStackParamList> = NativeStackScreenProps<
@@ -33,6 +34,7 @@ export type RootTabParamList = {
   MyRides:undefined;
   ChangePassword:undefined;
   Register:undefined;
+  StartRide:undefined;
 };
 
 export type RootTabScreenProps<Screen extends keyof RootTabParamList> = CompositeScreenProps<
