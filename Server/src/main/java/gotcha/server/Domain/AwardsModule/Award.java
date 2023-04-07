834ced7a83257493104ee020dbc2b9dce93f1912
Amit Moskovitz <moskoga02@gmail.com>
2023-04-07 20:26:11 +0300

    Add date field to award

--- Server/src/main/java/gotcha/server/Domain/AwardsModule/Award.java
+++ Server/src/main/java/gotcha/server/Domain/AwardsModule/Award.java
@@ -1,16 +1,21 @@
 package gotcha.server.Domain.AwardsModule;
 
+import java.time.LocalDate;
+
 public class Award {
     private int id;
     private String message;
     private String admin_email;
     private String[] emails;
 
+    private LocalDate date;
+
     public Award(int id, String message, String admin_email, String[] emails){
         this.id = id;
         this.message = message;
         this.admin_email = admin_email;
         this.emails = emails;
+        this.date = LocalDate.now(); // TODO: use another constructor for loading DB!
     }
 
     public int getId() {
@@ -27,5 +32,9 @@ public class Award {
 
     public String[] getEmails() {
         return emails;
+    }
+
+    public LocalDate getDate() {
+        return date;
     }
 }
