5b633d6bbdea688280a352ebc00cf9a0fc093ea2
Amit Moskovitz <moskoga02@gmail.com>
2023-04-09 12:20:12 +0300

    Users Window : change to 2 tables: riders & waiting RP serial numbers.

--- adminApp/API/UsersApi.js
+++ adminApp/API/UsersApi.js
@@ -3,9 +3,10 @@ import {CATCH, CONNECTION_ERROR} from "./AdvertismentsApi"
 import axios from '../assets/AxiosInstance';
 import {path} from "./Path"
 
-const ADD_USER_RP_PATH =path+"add_user_rp"
-const DELETE_USER_PATH = path+"delete_user"
-const VIEW_USER_PATH = path+"view_users"
+const ADD_USER_RP_PATH =path + "add_rp_serial_number"
+const DELETE_USER_PATH = path + "delete_user"
+const VIEW_USER_PATH = path + "view_users"
+const VIEW_WAITING_RP_PATH = path + "view_waiting_rp"
 
 
 export class UsersApi {
@@ -13,7 +14,7 @@ export class UsersApi {
     add_user_rp(user_rp_to_add) {
         return axios.get(ADD_USER_RP_PATH,
         {
-            params:{rp_serial_number: user_rp_to_add}
+            params:{rp_serial: user_rp_to_add}
         })
             .then(res => {
                 return new Response(res.data);
@@ -41,6 +42,15 @@ export class UsersApi {
                 .catch(res => Response.create(CATCH,true, CONNECTION_ERROR ));
         
     }
+    view_waiting_rp(){
+        return axios.get(VIEW_WAITING_RP_PATH)
+                .then(res => {
+                    return res.data;
+                })
+                .catch(res => Response.create(CATCH,true, CONNECTION_ERROR ));
+        
+    }
+    
 
 
 }
