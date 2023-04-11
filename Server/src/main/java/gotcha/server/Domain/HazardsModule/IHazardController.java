21a57c5b11483fab50fef004148a7947af0763d2
Amit Moskovitz <moskoga02@gmail.com>
2023-04-11 16:38:31 +0300

    Update IHazardController.java

--- Server/src/main/java/gotcha/server/Domain/HazardsModule/IHazardController.java
+++ Server/src/main/java/gotcha/server/Domain/HazardsModule/IHazardController.java
@@ -14,7 +14,7 @@ public interface IHazardController {
     void update_hazards(String hazard_info, int ride_id);
     List<StationaryHazard> get_hazards_in_route(Route route);
 
-    // Collection<StationaryHazardDAO> view_hazards();
+     Collection<StationaryHazardDAO> view_hazards();
 
 
 //    void update_hazard(StationaryHazard hazard, double size);
@@ -24,7 +24,7 @@ public interface IHazardController {
 //    List<StationaryHazard> get_hazards(HazardType type);
 //    List<StationaryHazard> get_hazards(String city);
 //    List<StationaryHazard> get_hazards(Location location, double radios);
-    List<StationaryHazard> view_hazards();
+//    List<StationaryHazard> view_hazards();
 
     void report_hazard(int hazardId) throws Exception;
 }
