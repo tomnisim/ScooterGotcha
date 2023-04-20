70f2a382e4ff243e0e242031a2dd0da9095b255e
tomnisim <tomnisi@post.bgu.ac.il>
2023-04-20 18:52:45 +0300

    update config thread

--- raspberryPi/Service/Service.py
+++ raspberryPi/Service/Service.py
@@ -15,7 +15,7 @@ from Config.Config_data import Config_data
 from GPSModule.GPSController import GPSController
 from PersistenceModule.PersistenceController import PersistenceController
 from RidesModule.RideController import RideController
-
+import time
 
 # Configure GPIO
 # this pin depends on the physical pin we will use
@@ -35,15 +35,23 @@ def manage_live_button():
     finally:
         GPIO.cleanup()
 
+def update_config():
+    while True:
+        Config_data()
+        time.sleep(3600)
+
 class Service:
     def __init__(self):
-        config_data = Config_data()
+        Config_data()
+        update_config_thread = threading.Thread(target=update_config)
+        update_config_thread.start()
         self._GPS_controller = GPSController.get_instance()
         self._camera_controller = CameraController.get_instance()
         self.alerter = VocalCreator() # #TODO: have to make switch case according the configuration file
         self.ride_controller = self.create_ride_controller(self._GPS_controller, self._camera_controller )
 
         live_button_thread = threading.Thread(target=manage_live_button)
+        live_button_thread.start()
 
     def create_ride_controller(self, gps_controller, camera_controller):
         curr_alerter = self.alerter.create_alerter()
