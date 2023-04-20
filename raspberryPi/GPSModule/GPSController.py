7b0447d5eafb5a107347ad3f650bc1d1dc2847c1
tomnisim <tomnisi@post.bgu.ac.il>
2023-04-20 18:46:05 +0300

    change get_location

--- raspberryPi/GPSModule/GPSController.py
+++ raspberryPi/GPSModule/GPSController.py
@@ -30,35 +30,22 @@ class GPSController:
 
     # Get the current location from the GPS module
     def get_location(self):
-        location = None
-        while location is None:
-            try:
-                data = self.gps_serial.readline().decode('ascii', errors='replace')
-                if data.startswith('$GPGGA'):
-                    msg = pynmea2.parse(data)
-                    lat = msg.latitude
-                    lng = msg.longitude
-                    location = (lat, lng)
-            except Exception as e:
-                print(f"Error: {e}")
-        return location
+        return ("34.856", "32.989")
+        # location = None
+        # while location is None:
+        #     try:
+        #         data = self.gps_serial.readline().decode('ascii', errors='replace')
+        #         if data.startswith('$GPGGA'):
+        #             msg = pynmea2.parse(data)
+        #             lat = msg.latitude
+        #             lng = msg.longitude
+        #             location = (lat, lng)
+        #     except Exception as e:
+        #         print(f"Error: {e}")
+        # return location
 
 
-    def get_city(self, location):
-        # Replace YOUR_API_KEY with your actual API key for the Google Maps Geocoding API
-        api_key = "YOUR_API_KEY"
-        # Construct the URL for the API request
-        url = "https://maps.googleapis.com/maps/api/geocode/json?latlng={},{}&key={}".format(location[0], location[1],
-                                                                                             api_key)
-        # Send the API request and get the response as JSON
-        response = requests.get(url).json()
-        # Parse the response and get the name of the city
-        for result in response["results"]:
-            for component in result["address_components"]:
-                if "locality" in component["types"]:
-                    return component["long_name"]
-        # If the city name couldn't be found, return None
-        return None
+
 
     #
     # def get_GPS_location(self):
