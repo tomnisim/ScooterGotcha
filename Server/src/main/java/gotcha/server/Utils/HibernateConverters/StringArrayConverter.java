42a12840c3904ce1cf93d12390efcb109cc206b3
Amit Moskovitz <moskoga02@gmail.com>
2023-05-01 14:24:20 +0300

    Update StringArrayConverter.java

--- Server/src/main/java/gotcha/server/Utils/HibernateConverters/StringArrayConverter.java
+++ Server/src/main/java/gotcha/server/Utils/HibernateConverters/StringArrayConverter.java
@@ -2,18 +2,27 @@ package gotcha.server.Utils.HibernateConverters;
 
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Converter
-public class StringArrayConverter implements AttributeConverter<String[], String> {
+public class StringArrayConverter implements AttributeConverter<List<String>, String> {
     private static final String DELIMITER = ",";
 
     @Override
-    public String convertToDatabaseColumn(String[] attribute) {
+    public String convertToDatabaseColumn(List<String> attribute) {
+        if (attribute == null || attribute.isEmpty()) {
+            return "";
+        }
         return String.join(DELIMITER, attribute);
     }
 
     @Override
-    public String[] convertToEntityAttribute(String dbData) {
-        return dbData.split(DELIMITER);
+    public List<String> convertToEntityAttribute(String dbData) {
+        if (dbData == null || dbData.trim().length() == 0) {
+            return new ArrayList<>();
+        }
+        return Arrays.asList(dbData.split(DELIMITER));
     }
 }
