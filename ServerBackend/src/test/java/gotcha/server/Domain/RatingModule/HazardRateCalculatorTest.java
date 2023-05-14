37e4ce414122707ac6cb68907bd9f1b5a31c2a29
Amit Moskovitz <moskoga02@gmail.com>
2023-05-14 15:35:46 +0300

    Add hazards & notifications tests

--- ServerBackend/src/test/java/gotcha/server/Domain/RatingModule/HazardRateCalculatorTest.java
+++ ServerBackend/src/test/java/gotcha/server/Domain/RatingModule/HazardRateCalculatorTest.java
@@ -2,6 +2,7 @@ package gotcha.server.Domain.RatingModule;
 
 import gotcha.server.Domain.HazardsModule.HazardType;
 import gotcha.server.Domain.HazardsModule.StationaryHazard;
+import gotcha.server.Utils.Formula;
 import gotcha.server.Utils.Location;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,6 +11,8 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+import org.apache.commons.lang3.builder.EqualsBuilder;
 
 class HazardRateCalculatorTest {
 
@@ -36,7 +39,7 @@ class HazardRateCalculatorTest {
     void rate_hazard_poleTree() {
         StationaryHazard hazard = new StationaryHazard(5, location, city, type1, size, null);
         double answer = hazard.getRate();
-        assertEquals(answer, 5+(size/2)*10);
+        assertEquals(answer, 5 + (size / 2) * 10);
     }
 
     @Test
@@ -44,7 +47,7 @@ class HazardRateCalculatorTest {
     void rate_hazard_pothole() {
         StationaryHazard hazard = new StationaryHazard(5, location, city, type2, size, null);
         double answer = hazard.getRate();
-        assertEquals(answer, 10+(size/2)*10);
+        assertEquals(answer, 10 + (size / 2) * 10);
     }
 
     @Test
@@ -53,7 +56,7 @@ class HazardRateCalculatorTest {
         double size = 16;
         StationaryHazard hazard = new StationaryHazard(5, location, city, type3, size, null);
         double answer = hazard.getRate();
-        assertEquals(answer, (size/2)*10);
+        assertEquals(answer, (size / 2) * 10);
     }
 
     @Test
@@ -65,5 +68,45 @@ class HazardRateCalculatorTest {
 
     @Test
     void update_hazard_formula() {
+    }
+
+    @Test
+    public void HazardRateCalculator() {
+        HazardRateCalculator expected = null;
+        HazardRateCalculator actual = new HazardRateCalculator();
+
+        assertTrue(EqualsBuilder.reflectionEquals(expected, actual, false, null));
+    }
+
+    @Test
+    public void get_instance() {
+        HazardRateCalculator expected = null;
+        HazardRateCalculator actual = HazardRateCalculator.get_instance();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void rate_hazard() {
+        HazardRateCalculator h = null;
+        StationaryHazard hazard = null;
+        double expected = 0.0D;
+        double actual = h.rate_hazard(hazard);
+
+        assertEquals(expected, actual, 0.0000001D);
+    }
+
+    @Test
+    public void set_formulasTODO() {
+        HazardRateCalculator h = null;
+        h.set_formulas();
+    }
+
+    @Test
+    public void update_hazard_formulaTODO() {
+        HazardRateCalculator h = null;
+        HazardType hazardType = null;
+        Formula formula = null;
+        h.update_hazard_formula(hazardType, formula);
     }
 }
