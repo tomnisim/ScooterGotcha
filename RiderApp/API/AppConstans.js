fcf474d19936e230d758880d5a0d5f44a7e0b627
Amit Moskovitz <moskoga02@gmail.com>
2023-05-14 15:37:35 +0300

    Fix icons

--- RiderApp/API/AppConstans.js
+++ RiderApp/API/AppConstans.js
@@ -4,26 +4,26 @@ export const CONNECTION_ERROR = "The system is not available right now, come bac
 export const CATCH = "CATCH";
 export const background = {uri: 'https://raw.githubusercontent.com/tomnisim/ScooterGotcha/main/adminApp/assets/background.png'};
 export const hazardIcon = L.icon({
-    iconUrl: 'https://raw.githubusercontent.com/tomnisim/ScooterGotcha/119-my-rides-screen/RiderApp/assets/warning.png',
+    iconUrl: 'https://raw.githubusercontent.com/tomnisim/ScooterGotcha/main/RiderApp/assets/warning.png',
     iconSize: [30, 30],
     iconAnchor: [15, 15]
   });
 
 
 export const originIcon = L.icon({
-    iconUrl: 'https://raw.githubusercontent.com/tomnisim/ScooterGotcha/119-my-rides-screen/RiderApp/assets/origin.png',
+    iconUrl: 'https://raw.githubusercontent.com/tomnisim/ScooterGotcha/main/RiderApp/assets/origin.png',
     iconSize: [30, 30],
     iconAnchor: [15, 15]
   });
 
 export  const destIcon = L.icon({
-    iconUrl: 'https://raw.githubusercontent.com/tomnisim/ScooterGotcha/119-my-rides-screen/RiderApp/assets/origin.png',
+    iconUrl: 'https://raw.githubusercontent.com/tomnisim/ScooterGotcha/main/RiderApp/assets/origin.png',
     iconSize: [30, 30],
     iconAnchor: [15, 15]
   });
 
   export  const liveLocationIcon = L.icon({
-    iconUrl: 'https://raw.githubusercontent.com/tomnisim/ScooterGotcha/119-my-rides-screen/RiderApp/assets/liveLocation.png',
+    iconUrl: 'https://raw.githubusercontent.com/tomnisim/ScooterGotcha/main/RiderApp/assets/liveLocation.png',
     iconSize: [30, 30],
     iconAnchor: [15, 15]
   });
