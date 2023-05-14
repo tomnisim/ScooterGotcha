b6021fd696edda45f708029fd7cff281e42f69dd
Amit Moskovitz <moskoga02@gmail.com>
2023-05-14 16:00:07 +0300

    Update IHazardControllerTest.java

--- ServerBackend/src/test/java/gotcha/server/Domain/HazardsModule/IHazardControllerTest.java
+++ ServerBackend/src/test/java/gotcha/server/Domain/HazardsModule/IHazardControllerTest.java
@@ -58,7 +58,7 @@ class IHazardControllerTest {
                 new StationaryHazardRPDTO(20, new LocationDTO(location2),Type.name(), null)
         ));
         assertDoesNotThrow(() -> hazardController.update_hazards(hazards,1, City));
-        assertFalse(hazardController.view_hazards().size() == hazards.size());
+        assertTrue(hazardController.view_hazards().size() == hazards.size());
     }
 
     @Test
