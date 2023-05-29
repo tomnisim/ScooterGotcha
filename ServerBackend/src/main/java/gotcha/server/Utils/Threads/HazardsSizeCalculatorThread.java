53b24fb6d78206aae08933d7fa9fd4650b00c4f6
Amit Moskovitz <moskoga02@gmail.com>
2023-05-29 16:05:29 +0300

    Update HazardsSizeCalculatorThread.java

--- ServerBackend/src/main/java/gotcha/server/Utils/Threads/HazardsSizeCalculatorThread.java
+++ ServerBackend/src/main/java/gotcha/server/Utils/Threads/HazardsSizeCalculatorThread.java
@@ -3,6 +3,7 @@ package gotcha.server.Utils.Threads;
 
 import gotcha.server.Domain.HazardsModule.HazardController;
 import gotcha.server.Domain.HazardsModule.StationaryHazard;
+import gotcha.server.Domain.s3.S3Service;
 import gotcha.server.Utils.Logger.SystemLogger;
 
 import java.util.List;
@@ -13,18 +14,34 @@ import static gotcha.server.Utils.PotholeWidthDetection.detect;
 public class HazardsSizeCalculatorThread implements Runnable {
     private HazardController hazardController;
     private SystemLogger systemLogger;
-    public HazardsSizeCalculatorThread(HazardController hazardController, SystemLogger systemLogger) {
+    private S3Service s3Service;
+    public HazardsSizeCalculatorThread(HazardController hazardController, SystemLogger systemLogger, S3Service s3Service) {
         this.hazardController = hazardController;
         this.systemLogger = systemLogger;
+        this.s3Service = s3Service;
     }
 
     @Override
     public void run() {
+        this.systemLogger.add_log("Start Update Hazards Size");
         List<StationaryHazard> hazards = this.hazardController.get_hazards();
         for (StationaryHazard hazard : hazards){
-            hazard.setSize(detect(hazard.getPhotoS3Key()));
+            if (hazard.getSize() <= 0){
+                try
+                {
+                    byte[] frame = s3Service.getImage(hazard.getPhotoS3Key());
+                    double[] sizes = detect(frame);
+                    hazardController.update_hazard(hazard, sizes[0]);
+                    this.systemLogger.add_log(String.format("Update Hazard with id : %s to Size: %s", hazard.getId(), hazard.getSize()));
+                }
+                catch (Exception e){
+                    this.systemLogger.add_log(String.format("Failed Update Hazard with id : %s", hazard.getId()));
+                    this.systemLogger.add_log(e.getMessage());
+                }
+
+            }
         }
-        this.systemLogger.add_log(String.format("Report hazards with ids");
+
     }
 
 
