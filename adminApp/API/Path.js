410f9dbd9dfcb843a116edc5b695df0b957f9c0c
sashajke <uzelevsk@post.bgu.ac.il>
2023-06-10 16:04:39 +0300

    test

--- adminApp/API/Path.js
+++ adminApp/API/Path.js
@@ -1,2 +1,2 @@
-export const path = "https://16.16.199.51:5050/"
+export const path = "https://16.16.199.51:443/"
 export const background = {uri: 'https://raw.githubusercontent.com/tomnisim/ScooterGotcha/main/adminApp/assets/background.png'};
