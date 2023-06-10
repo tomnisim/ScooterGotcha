f9df8118874a5cadf640f92fd8c27da19399bb4a
sashajke <uzelevsk@post.bgu.ac.il>
2023-06-10 15:47:46 +0300

    added ssl

--- adminApp/API/Path.js
+++ adminApp/API/Path.js
@@ -1,2 +1,2 @@
-export const path = "http://16.170.252.135:5050/"
+export const path = "https://16.16.199.51:5050/"
 export const background = {uri: 'https://raw.githubusercontent.com/tomnisim/ScooterGotcha/main/adminApp/assets/background.png'};
