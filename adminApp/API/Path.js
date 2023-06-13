9d40b0e5165ded8e2685d04f0ec9faf3570f9313
sashajke <62395693+sashajke@users.noreply.github.com>
2023-06-13 18:27:35 +0300

    Update Path.js

--- adminApp/API/Path.js
+++ adminApp/API/Path.js
@@ -1,2 +1,2 @@
-export const path = "/"
+export const path = "https://serverbackend-udyb.onrender.com/"
 export const background = {uri: 'https://raw.githubusercontent.com/tomnisim/ScooterGotcha/main/adminApp/assets/background.png'};
